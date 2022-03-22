4ed1807bee74db031bbf63210803ef4bc23f3798
polinaprinii <x21137757@student.ncirl.ie>
2022-03-22 23:38:18 +0000

    Update crossRoad.py

--- crossRoad.py
+++ crossRoad.py
@@ -1,4 +1,20 @@
 """
-Object: Cross road, outlining starting point and specifying first car distance from both north and south bound.
+Creating two object which holds the following:
+- Main road, outlining the start and end point of the road.
+- Local road, outlining the start and end point of the road.
 """
+
+from scipy.spatial import distance
+
+class mainRoad(object):
+
+    def __init__(self,  startN, endN):
+        self.startN = startN
+        self.endN = endN
+
+class localRoad(object):
+
+    def __init__(self, startL, endL):
+        self.startL = startL
+        self.endL = endL
 
