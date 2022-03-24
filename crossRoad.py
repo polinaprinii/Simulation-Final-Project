ada304352968b1dba3c7ecf4634db9a0d53c6913
polinaprinii <x21137757@student.ncirl.ie>
2022-03-24 23:03:05 +0000

    Update crossRoad.py
    Re-evaluated the definition of the environment in prep for the simulation.

--- crossRoad.py
+++ crossRoad.py
@@ -1,20 +1,30 @@
 """
-Creating two object which holds the following:
-- Main road, outlining the start and end point of the road.
-- Local road, outlining the start and end point of the road.
+Setting up the environment of which the simulation will run.
 """
+PASS = 0.05 # Amount of seconds it takes to pass the intersection whilst driving.
+CROSS = 0.8 # Amount of seconds it takes to cross the intersection from stopping position.
+class CrossRoad(object):
+    """ A cross road consists of two roads, main and local.
+    The main road has priority whilst the local road cuts through the main."""
+    def __init__(self, env, north, south, west, east, passtime, crosstime):
+        self.env = env
+        self.north = north
+        self.south = south
+        self.west = west
+        self.east = east
+        self.passtime = passtime
+        self.crosstime = crosstime
 
-from scipy.spatial import distance
+    def passing(self, car):
+        """ The driving process of a car on the main road past the cross road."""
+        yield self.env.timeout(PASS)
+        print("Main car passed at %d" % self.env.now)
 
-class mainRoad(object):
+    def crossing(self, car):
+        """ The driving process of a car of the local road to cross the cross road."""
+        yield self.env.timeout(CROSS)
+        print("Local car crossed at %d" % self.env.now)
 
-    def __init__(self,  startN, endN):
-        self.startN = startN
-        self.endN = endN
 
-class localRoad(object):
 
-    def __init__(self, startL, endL):
-        self.startL = startL
-        self.endL = endL
 
